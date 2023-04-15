82387bddfb8754513056225de520d85792722e00
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-04-15 10:16:05 +0300

    fixed

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,17 @@ const num = prompt(`Number`)
 const degree = prompt(`Degree`)
 
   function x (num, degree = 1){
-    if (typeof num !== `number` || typeof degree !== `number`){
-        alert(`some error`)
+    if (isNaN(num)|| isNaN(degree)){
+        return`some error`
     }
     return Math.pow (num, degree)
   }
 
   let a = Number (num)
 let b = Number (degree)
-
+if (num === null || degree === null){
+  alert(`wrong type`)
+} else {
   const result = x (a, b)
-
-alert (`${result}`)
+  alert(result)
+}
